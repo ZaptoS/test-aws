bf81b9b8726f50115b337248c42b047f5e115ce8
Ivan Lavrenov <ivan.lavrenov@softeq.com>
2016-07-21 12:06:05 +0300

    test

--- src/main/java/com/springapp/mvc/HelloController.java
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HelloController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
-		//todo add comments one more time
+		//todo add comments one more time 2
 		model.addAttribute("message", "Hello world!");
 		return "hello";
 	}
